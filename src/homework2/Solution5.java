abeae1f9b75fe23462be0fec78e78e7a03ae0a40
Nick Maslov <waadamas@gmail.com>
2020-09-07 13:49:14 +0300

    Поправил замечания

--- src/homework2/Solution5.java
+++ src/homework2/Solution5.java
@@ -3,25 +3,19 @@ package homework2;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import static java.util.Arrays.sort;
+
 public class Solution5 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Введите число 1:");
-        BufferedReader bufferedReader1 = new BufferedReader(new InputStreamReader(System.in));
-        int x1 = Integer.parseInt(bufferedReader1.readLine());
-        System.out.println("Введите число 2:");
-        BufferedReader bufferedReader2 = new BufferedReader(new InputStreamReader(System.in));
-        int x2 = Integer.parseInt(bufferedReader2.readLine());
-        System.out.println("Введите число 3:");
-        BufferedReader bufferedReader3 = new BufferedReader(new InputStreamReader(System.in));
-        int x3 = Integer.parseInt(bufferedReader3.readLine());
-
-        if (x1 <= x2 && x1 <= x3) {
-            System.out.println("Наименьшее число = " + x1);
-        } else if (x2 <= x1 && x2 <= x3) {
-            System.out.println("Наименьшее число = " + x2);
-        } else {
-            System.out.println("Наименьшее число = " + x3);
+        int[] array = new int[3];
+        for (int i = 1; i < 4; i++) {
+            System.out.println("Введите число " + i + ":");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            int x = Integer.parseInt(bufferedReader.readLine());
+            array[i-1] = x;
         }
+        sort(array);
+        System.out.println("Наименьшее число: " + array[0]);
 
     }
 
--- src/homework2/Solution7.java
+++ src/homework2/Solution7.java
@@ -5,26 +5,36 @@ import java.io.InputStreamReader;
 
 public class Solution7 {
     public static void main(String[] args) throws Exception {
-        double tax;
+        double tax = 0;
+        String city = "";
         double time = 10;
         System.out.println("Введите число:");
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String x = bufferedReader.readLine();
-        if (x.equals("905")) {
-            tax = 4.15;
-            System.out.println("Москва. Стоимость разговора: " + tax * time); // так как параметр умножения на 10 статичный, было бы логично запихнуть готовые значения в стринг
-        } else if (x.equals("194")) {
-            tax = 1.98;
-            System.out.println("Ростов. Стоимость разговора: " + tax * time);
-        } else if (x.equals("491")) {
-            tax = 2.69;
-            System.out.println("Краснодар. Стоимость разговора " + tax * time);
-        } else if (x.equals("800")) {
-            tax = 5.00;
-            System.out.println("Киров. Стоимость разговора " + tax * time);
+        int x = Integer.parseInt(bufferedReader.readLine());
+        switch (x) {
+            case 905:
+                tax = 4.15;
+                city = "Москва";
+                break;
+            case 194:
+                tax = 1.98;
+                city = "Ростов";
+                break;
+            case 491:
+                tax = 2.69;
+                city = "Краснодар";
+                break;
+            case 800:
+                tax = 5.00;
+                city = "Киров";
+                break;
+            default:
+                break;
+        }
+        if (city.equals("")) {
+            System.out.println("Такой код отсутствует!");
         } else {
-            System.out.println("Такой код отсутствует");
+            System.out.println(city + ". Стоимость разговора: " + tax * time);
         }
     }
-
 }
