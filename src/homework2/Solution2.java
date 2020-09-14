44e86b931c6d54d9b64d3c03a217a484d835ec0f
Nick Maslov <waadamas@gmail.com>
2020-09-14 19:20:37 +0300

    Поправил замечания по заданию номер 2

--- src/homework2/Solution2.java
+++ src/homework2/Solution2.java
@@ -3,20 +3,22 @@ package homework2;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import static com.sun.org.apache.xalan.internal.lib.ExsltStrings.split;
+
 public class Solution2 {
     public static void main(String[] args) throws Exception {
         System.out.println("Введите число:");
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String str = bufferedReader.readLine();             //Сначала сохраняем как строку что бы убедиться что введено три символа
+        String str = bufferedReader.readLine();
         if (str.length() != 3) {
             System.out.println("Число должно быть трёхзначным!");
         } else {
-            int num = Integer.parseInt(str);                  //Переводим в int
-            int d1 = num % 10;
-            int d2 = num % 100 / 10;
-            int d3 = num / 100;
-            int sum = d1 + d2 + d3;
-            System.out.println("Сумма цифр числа " + num + ": " + sum);
+            int sum = 0;
+            String[] parts = str.split("");
+            for (String value : parts){
+                sum = sum + Integer.parseInt(value);
+            }
+            System.out.println(sum);
         }
 
     }
